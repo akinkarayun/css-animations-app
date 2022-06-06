88c421e2f6df606a7a89e00b32242e29bc53f273
Akin Karayun <akinkarayun1@hotmail.com>
2022-06-06 17:38:37 +0300

    added new css animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //first app
+    // <section>
+    //   <div className="pattern">
+    //     <div className="face face1"></div>
+    //     <div className="face face2"></div>
+
+    //   </div>
+    // </section>
+    ////////////////////////////////////
+
+    //second app
+    // <body>
+    //   <div className='loader'>
+    //     <span></span>
+    //     <span></span>
+    //     <span></span>
+    //   </div>
+    // </body>
+
+    ///third app
+
+    // <body>
+    //   <div className='container'>
+    //     <div className='infinite'>
+    //       <div className='shadow'>
+    //       </div>
+    //       <div className='box'>
+    //         <div className='square'>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </body>
+
+    <body>
+      <div className="container">
+        <div className="plate"></div>
+        <div className="cup">
+          <div className="top">
+            <div className="vapour">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className="circle">
+              <div className="tea"></div>
+            </div>
+          </div>
+          <div className="handle"></div>
+        </div>
+      </div>
+    </body>
+  )
 }
 
-export default App;
+export default App
